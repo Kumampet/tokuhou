1932fa0014a1d6236c4fc221ccc08067f8d2ecaf
Kumampet <kumampet@gmail.com>
2020-03-27 22:26:47 +0900

    音だけ実装できなかった。これでとりあえず完成。

--- js/result.js
+++ js/result.js
@@ -3,6 +3,7 @@ $(function () {
     var dom;
     var str;
 
+
     $('#done').on('click', function () {
         $('input[id="result"]').attr('name', 'text' + num + '');
         $('#input').fadeOut(1000).hide(0, function () {
@@ -17,17 +18,10 @@ $(function () {
             }
 
             $('#output').fadeIn(200);
+
             $('.s').each(function (i) {
-                $('.str' + (i + 1) + '').delay(5000 * i).fadeIn(0).delay(5100).fadeOut(200);
+                $('.str' + (i + 1) + '').delay(5000 * i).fadeIn(0).delay(5000).fadeOut(200);
             });
-
-            //最後のオチのデータをセットする
-            // str = $('input[name="result').val();
-            // dom = "<h1 class='out-result'>" + str + "</h1>";
-            // $('#content').append(dom);
-            // $('.out-result').hide();
-
-
         });
     });
 });
